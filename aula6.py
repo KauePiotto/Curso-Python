8c290fe6de5a27dd7dae56ac42edaf97ad625877
KauePiotto <kauepiotto@gmail.com>
2025-12-02 17:50:09 -0300

    Sexta aula de Python

--- aula6.py
+++ aula6.py
@@ -3,3 +3,8 @@
 # e o ato de converter um tipo em outro
 # tipos imutaveis e primitivos:
 # str, int, float, bool
+
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11) + 'b')

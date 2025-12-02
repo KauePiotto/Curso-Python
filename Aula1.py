4853e7a7f435432672e49da9e74224770d7b5dba
KauePiotto <kauepiotto@gmail.com>
2025-12-01 22:46:53 -0300

    Primeira aula de Python

--- Aula1.py
+++ Aula1.py
@@ -1,3 +1,24 @@
-print("Hello Mundo!")
-print("Opa, editei.")
-print("Opa, editei.")
+# Permite escrever um comentario em uma linha
+print(123) # Na Frente
+# Abaixo 
+print(456)
+
+#DocString serve para comentar mais de uma linha
+
+"""
+Serve 
+para 
+comentar 
+mais 
+de 
+uma 
+linha
+"""
+
+'''
+Tabom 
+pode 
+usar 
+para 
+escrever suas notas ou seus comentarios no codigo
+'''

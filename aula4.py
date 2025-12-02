562c24206d33ed238cad22568010a9607706fd91
KauePiotto <kauepiotto@gmail.com>
2025-12-01 23:47:45 -0300

    Quarta aula de Python (Aprendendo sobre valores Inteiros e Float)

---
+++ aula4.py
@@ -0,0 +1,25 @@
+# Tipo int e float 
+# int -> Numero inteiro
+# positivo ou negativo. int sem sinal e considerado
+# positivo
+
+print(11) # int 
+print(-11) # int
+print(0)
+
+# flaot -> Numero com ponto flutuante 
+# o tipo float representa qualquer numero 
+# positivo ou negativo com ponto flutuante. 
+# float sem sinal e considerado positivo
+
+print(1.1, 10.11)
+print(0.0, -1.5)
+
+# A funcao type mostra o tipo que o python 
+# inferiu ao valor.
+
+print(type('Otavio'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))
+
+# Valores de variavel int e float sao de tipos primitivos em python

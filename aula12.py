a68533c278307e18b48f288c32e0ffbf60ba1886
KauePiotto <kauepiotto@gmail.com>
2025-12-02 19:31:43 -0300

    Decima segunda aula de Python (Exercicio 2 aprendendo fazer o calculo do IMC)

---
+++ aula12.py
@@ -0,0 +1,11 @@
+#Exercio 2 fazendo calculo do IMC
+
+nome = 'Kaue Piotto'
+altura = 1.80
+peso = 70
+imc = peso / altura ** 2
+
+
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu IMC e', imc)

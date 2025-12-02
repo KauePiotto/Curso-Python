7289f7715ef037f3409436dbd13f756f6987a7d0
KauePiotto <kauepiotto@gmail.com>
2025-12-02 18:51:18 -0300

    Decima aula de Python (Aprendendo sobre concatenacao e repeticao com operadores logicos)

---
+++ aula10.py
@@ -0,0 +1,9 @@
+# Aula de concatenacao e repeticao com operadores logicos
+
+concatenacao = 'Luiz' + ' ' + 'Otavio'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_luiz = 3 * 'Luiz'
+print(a_dez_vezes)
+print(tres_vezes_luiz)

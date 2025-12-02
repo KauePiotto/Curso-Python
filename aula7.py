e40b258d81a34d666c496ec99931f1340057b795
KauePiotto <kauepiotto@gmail.com>
2025-12-02 18:08:18 -0300

    Setima aula de Python (Aprendendo sobre variavel)

---
+++ aula7.py
@@ -0,0 +1,21 @@
+# Variavel sao usadas para salvar algo na memoria do computador
+# PEP8: inicie variavel com letras minusculas, pode usar 
+# numeros e underline _.
+# O sinal de = e o operador de atribuicao. Ele e usado para 
+# atribuir um valor a um nome (variavel).
+# Uso: nome_variavel = expressao.
+
+nome_completo = 'Kaue Piotto de Lima Freire'
+soma_dois_mais_dois = 2 + 2
+int_um = bool('1')
+print(nome_completo); #Chama a variavel nome_completo
+print(soma_dois_mais_dois); #Chama a variavel 
+print(int_um, type(int_um))
+
+
+nome = 'Luiz'
+idade = 30
+maior_de_idade = idade >= 18
+
+print('Nome:', nome, 'Idade:', idade, "Maior de idade:", maior_de_idade)
+print("E maior de idade?", maior_de_idade)

c772f0a1bbceb8da9a1eb5035e1d218c65ab546d
KauePiotto <kauepiotto@gmail.com>
2025-12-01 23:25:31 -0300

    Terceira aula de Python (Aprendendo sobre String)

---
+++ aula3.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguem de Programacao 
+Tipo de tipagem = Dinamica / Forte
+str -> strint -> texto
+Strings sao textos que estao dentro de aspas
+"""
+print(1234)
+
+#Aspas simples 
+print('Luiz Otavio')
+print(1, 'Luiz Otavio')
+
+#Aspas duplas
+print("Luiz Otavio")
+print(2, "Luiz 'Otavio'")
+ 
+#Escape
+print("Luiz \"Otavio\"")
+
+#r 
+print(r"Luiz \"Otavio\"")

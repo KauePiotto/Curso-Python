f9416d35fbf20fdcb6dc8340b591b56fc413cc37
KauePiotto <kauepiotto@gmail.com>
2025-12-02 00:02:08 -0300

    Quinta aula de Python (Aprendendo sobre variavel boolean)

---
+++ aula5.py
@@ -0,0 +1,18 @@
+# Tipo de dado bool (boolean)
+# ao questionar algo em um programa,
+# so existem duas respostas possiveis:
+# sim (True) ou nao (False).
+# Existem varios operadores para "questionar".
+# Dentre eles o ==, que e um operador logico que
+# questiona se um valor e igual o outro
+
+print (10 == 10) # Sim => True (Verdadeiro)
+print (10 == 11) # Nao => False (Falso)
+
+print (type(True))
+print (type(False))
+
+print (type(10 == 11))
+print (type(10 == 10)) 
+
+#Tipo de variavel booleana so serve para voce questionar o sistema de alguma coisa

1a41363c8f9d065e68d273b2f7e88086cf4d1489
KauePiotto <kauepiotto@gmail.com>
2025-12-02 18:39:06 -0300

    Nona aula de Python (Introduacao a operadores aritmeticos)

---
+++ aula9.py
@@ -0,0 +1,28 @@
+#Introducao a operadores aritimeticos
+
+adicao = 10 + 10
+print('Adicao', adicao)
+
+subtracao = 10 - 5
+print('Subtracao', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicacao', multiplicacao)
+
+divisao = 10 / 3  # float
+print('Divisao', divisao)
+
+divisao_inteira = 10 // 3
+print('Divisao inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciacao', exponenciacao)
+
+modulo = 55 % 2 # resto divisao
+print('Modulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)

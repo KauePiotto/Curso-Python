cee2027341b29a7f66fcbc2e52752c972c3e9a59
KauePiotto <kauepiotto@gmail.com>
2025-12-04 00:24:12 -0300

    aula 20 de Python (Exercicio 3 programando usando if e comparacao)

---
+++ aula20.py
@@ -0,0 +1,9 @@
+# Exercio 3 de programacao usando if e comparacao
+
+primeiro_Valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_Valor >= segundo_valor: 
+    print(f'{primeiro_Valor=} e maior ou igual ao {segundo_valor=} ')
+else:
+    print(f'{segundo_valor=} e maior do que {primeiro_Valor=}')    

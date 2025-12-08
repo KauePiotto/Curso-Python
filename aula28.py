b3d20137dcc57c25fd6c775f0c97ffcc14774204
KauePiotto <kauepiotto@gmail.com>
2025-12-08 19:09:05 -0300

    aulas 28 de Python (Exercicio 4 para testar seu conhecimento)

---
+++ aula28.py
@@ -0,0 +1,19 @@
+# Exercicio 4 para testa conhecimento
+
+"""
+Exercicio 
+Peca ao usuario para digitar seu nome
+peca ao usuario para digitar sua idade
+Se o nome e idade forem digitados:
+     Exiba:
+        Seu nome e {nome}
+        Seu nome invertido e {nome invertido}
+        Seu nome contem (ou nao) espacos
+        Seu nome tem {n} letras
+        A primeira letra do seu nome e {letra}
+        A ultima letra do seu nome e {letra}
+Se nada for digitado em nome ou idade: 
+    Exiba:
+      "Desculpa, voce deixou campos vazios."
+"""
+

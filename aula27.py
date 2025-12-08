208453b7946d99f10268f1d15816ce7aa6fe82f7
KauePiotto <kauepiotto@gmail.com>
2025-12-08 18:58:04 -0300

    aulas 27 de Python (Aprendendo sobre fatiamento de strings)

---
+++ aula27.py
@@ -0,0 +1,19 @@
+# Aprendendo sobre fatiamento de strings
+
+"""
+Fatiamento de strings 
+ 012345678
+ Ola Mundo
+-987654321
+Fatiamento [i (comeco):f (meio):p (fim)] [::]
+Obs.: a funcao len retorna a qtd
+de caracteres da str
+"""
+
+variavel = "Ola Mundo"
+print(len(variavel))
+
+# Serve para checar o tamanho da string
+
+variavel = "Ola Mundo"
+print(variavel[-1: -10 : -2])

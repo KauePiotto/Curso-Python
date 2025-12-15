c6c9bde73a05aa5c1f994bb7c6547c2296e89fbf
KauePiotto <kauepiotto@gmail.com>
2025-12-15 20:55:52 -0300

    aulas 28 de Python (Exercicio 4 para testar seu conhecimento)

--- aula28.py
+++ aula28.py
@@ -16,4 +16,22 @@ Se nada for digitado em nome ou idade:
     Exiba:
       "Desculpa, voce deixou campos vazios."
 """
+entrada_nome = input('Digite seu nome: ').strip()
+entrada_idade = input('Digite sua idade: ').strip()
 
+
+if(entrada_nome and entrada_idade):
+    nome_invertido = entrada_nome[::-1]
+    contem_espacos = 'sim' if ' ' in entrada_nome else 'nao'
+    numero_letras = len(entrada_nome.replace(' ', ''))
+    primeira_letra = entrada_nome[0]
+    ultima_letra = entrada_nome[-1]
+
+    print(f'Seu nome e {entrada_nome}')
+    print(f'Seu nome invertido e {nome_invertido}')
+    print(f'Seu nome contem espacos? {contem_espacos}')
+    print(f'Seu nome tem {numero_letras} letras')
+    print(f'A primeira letra do seu nome e {primeira_letra}')
+    print(f'A ultima letra do seu nome e {ultima_letra}')
+else:
+    print('Desculpa, voce deixou campos vazios.')
